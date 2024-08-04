2f329e6c6c8146301902e027233ba6c496e9e496
mizuki-boop <149584819+mizuki-boop@users.noreply.github.com>
2024-08-04 12:41:28 +0900

    Add files via upload

---
+++ kadai_026/Jyanken_Chapter26.java
@@ -0,0 +1,65 @@
+package kadai_026;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter26 {
+	
+
+	//　自分のじゃんけんの手を入力するメソッド
+	public String getMyChoice() {
+		System.out.println("自分のじゃんけんの手を入力しましょう\n"
+				+ "グーはrockのrを入力しましょう\n"
+				+ "チョキはscissorsのsを入力しましょう\n"
+				+ "パーはpaperのpを入力しましょう");
+		
+		Scanner scanner= new Scanner(System.in);
+			String myinput=scanner.next();
+			
+			if(myinput.equals("r")||myinput.equals("s")||myinput.equals("p")) {
+				scanner.close();
+				return myinput;
+			}else {
+				System.out.println("じゃんけんの手ではありません。再入力してください。");
+				return getMyChoice();
+			}
+
+	}
+	
+	
+//　対戦相手のじゃんけんの手を乱数で選ぶメソッド
+	public String getRandom() {
+		String[] rspArray= {"r", "s", "p"}; 
+		String yourinput=rspArray[(int) Math.floor(Math.random()*3)];
+		return yourinput;
+	}
+	
+	
+// じゃんけんを行うメソッド
+	public void playGame() {
+		
+//コレクションの作成
+		HashMap<String,String> rspMap = new HashMap<String,String>();
+	      rspMap.put("r","グー");
+	      rspMap.put("s","チョキ");
+	      rspMap.put("p","パー");
+			
+//自分と対戦相手のじゃんけんの手を出力する
+	      String me=new String(getMyChoice());
+	      String you=new String(getRandom());
+	      
+System.out.println("自分の手は" +rspMap.get(me)+",対戦相手の手は" + rspMap.get(you));
+	      
+//equals()メソッドで自分と対戦相手のじゃんけんの手を比較する	      
+	      if(me.equals(you)) {
+	         System.out.println("あいこです");
+	      } else if(me=="グー"&&you=="チョキ"||me=="チョキ"&&you=="パー"||me=="パー"&&you=="グー"){
+	    	  System.out.println("自分の勝ちです");
+	      } else{
+	    	  System.out.println("自分の負けです");
+	      }
+	      
+	
+	}
+
+}
